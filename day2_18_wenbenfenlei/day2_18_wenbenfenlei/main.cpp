5e4561715a6f2d2cfbcb0a15af17122163db4dac
LiuZiQiao <1844861420@qq.com>
2019-03-10 11:18:42 +0800

    封装使用接口

--- day2_18_wenbenfenlei/day2_18_wenbenfenlei/main.cpp
+++ day2_18_wenbenfenlei/day2_18_wenbenfenlei/main.cpp
@@ -1,15 +1,17 @@
 #include"TextSimilarity.h"
 #include<iostream>
 using namespace std;
-int main(int argc,char ** argv)
+
+void Demo(const char* dict, const char* f1, const char* f2)
 {
-	TextSimilarity ts("dict");
-	TextSimilarity::wordFreq wf = ts.getWordFreq("test.txt");
-	TextSimilarity::wordFreq wf1 = ts.getWordFreq("test1.txt");
+
+	TextSimilarity ts(dict);
+	TextSimilarity::wordFreq wf = ts.getWordFreq(f1);
+	TextSimilarity::wordFreq wf1 = ts.getWordFreq(f2);
 
 	vector<std::pair<std::string, int>> vec = ts.sortByValueReverse(wf);
 	vector<std::pair<std::string, int>> vec1 = ts.sortByValueReverse(wf1);
-	
+
 	TextSimilarity::wordSet wset;
 	ts.selectAimWords(vec, wset);
 	ts.selectAimWords(vec1, wset);
@@ -31,5 +33,32 @@ int main(int argc,char ** argv)
 
 	double cons = ts.consine(vect, vect1);
 
-	cout << cons << endl;
+	cout <<f1<<"and "<<f2<<"simiary:  " <<cons << endl;
+}
+
+
+void Usage()
+{
+	std::cout<<"Please Input dictPath and Two FilePath:" << std::endl;
+}
+
+int main(int argc,char ** argv)
+{
+	if (argc != 4)
+	{
+		Usage();
+	//	exit(0);
+	}
+
+	const char* dictpath = argv[1];
+	const char* filepath1 = argv[2];
+	const char* filepath2 = argv[3];
+//	std::cin >> dictpath >> filepath1 >> filepath2;
+
+	//const char*  dictpath = "dict";
+	//const char*  filepath1 = "test.txt";
+	//const char*  filepath2 = "test1.txt";
+
+	Demo(dictpath,filepath1, filepath1);
+	return 0;
 }
